04e9235b452498befc619c322b874a6991acf810
Niall.Russell <niallr2@gmail.com>
2024-02-07 16:10:23 +0000

    Week 3 accounts task

--- accounts.py
+++ accounts.py
@@ -4,4 +4,18 @@
 #Please enter an 10 digit account number: 1234567890
 #XXXXXX7890
 
-fullnumber = input
+def inputnumber():
+    fullnumber = input("Please enter a 10 digit account number: ")
+    listnumber = list(fullnumber)
+    if len(listnumber) == 10:
+        for i, v in enumerate(listnumber):
+            if i <= 5:
+                listnumber[i] = "X"
+        xnumber = ''.join(listnumber)
+        print(xnumber)                
+    else:
+        print("Number must be 10 digits long")
+        
+inputnumber()
+
+
