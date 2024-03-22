aea1b59eaa8902e84e512bb41929d398c008cb3d
Niall.Russell <niallr2@gmail.com>
2024-03-22 09:44:13 +0000

    no argument error

--- ecount.py
+++ ecount.py
@@ -9,18 +9,23 @@ import sys
 #Start count at 0
 total = 0
 
-#trying to open file and throwing an error message if does not exist
-#https://stackoverflow.com/questions/57007680/how-to-handle-the-exception-when-input-file-does-not-exists-in-python
-try: 
-    #Open file in read mode- this will open first file entered into command line after the programme file 
-    with open(sys.argv[1], "r") as f:
-        #Read each line
-        for line in f:
-            #Store total "e" count for each line in variable "x"
-            x = line.count("e")
-            #Add x to the total
-            total = total + x
-    #print total
-    print(total)
-except FileNotFoundError:
-    print('File does not exist')
+#Checking there is an argument in the command line. The programme name is the first argument, 
+#so no argument is given when length is 1 or below- https://www.geeksforgeeks.org/how-to-use-sys-argv-in-python/
+if len(sys.argv) > 1:
+    #trying to open file and throwing an error message if does not exist
+    #https://stackoverflow.com/questions/57007680/how-to-handle-the-exception-when-input-file-does-not-exists-in-python
+    try: 
+        #Open file in read mode- this will open first file entered into command line after the programme file 
+        with open(sys.argv[1], "r") as f:
+            #Read each line
+            for line in f:
+                #Store total "e" count for each line in variable "x"
+                x = line.count("e")
+                #Add x to the total
+                total = total + x
+        #print total
+        print(total)
+    except FileNotFoundError:
+        print('File does not exist')
+else:
+    print("No argument in command line")
