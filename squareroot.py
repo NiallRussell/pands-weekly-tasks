95d9af8e5c26e5a01bfe5e146abf3aba7e775671
Niall.Russell <niallr2@gmail.com>
2024-03-25 12:15:52 +0000

    changing number of iterations

--- squareroot.py
+++ squareroot.py
@@ -1,25 +1,19 @@
-#Write a program that takes a positive floating-point number as input and outputs an approximation of its square root.
-#You should create a function called <tt>sqrt</tt> that does this.
-#I am asking you to create your own sqrt function and not to use the built in functions x ** .5 or math.sqrt(x).
-#This is to demonstrate that you can research and code a process (If you really needed the square root you would use one of the above methods). I suggest that you look at the newton method at estimating square roots. 
-#This is a more difficult task than some of the others, but will be marked equally, so only do as much work on this as you feel comfortable.
+#This program takes a positive floating-point number as input and outputs an approximation of its square root
+#Author: Niall Russell
 
-#$ python squareroot.py
-#Please enter a positive number: 14.5
-#The square root of 14.5 is approx. 3.8.
-
-#https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo
-#defining function
+#Using Newton's method for estimating square roots: https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo
+#Defining function
 def sqrt(): 
-    #promting input of number
+    #Promting input of number
     number = float(input("Please enter a positive number:"))
-    #first guess will be at least half the value of the number
+    #First guess will be at least half the value of the number
     nth_guess = number/2
-    #define a number of iterations- I could not find a consensus online as to how many
-    for i in range (10):
+    #Define a number of iterations- I could not find a consensus online as to how many, 
+    #however, this source suggests that 7 iterations is enough to produce more than 60 accurate digits- https://math.mit.edu/~stevenj/18.335/newton-sqrt.pdf
+    for i in range (7):
         #Newtown's formula
         nth_guess = (nth_guess + (number/nth_guess))/2 
-    #print solution
+    #Print solution
     print(f'The square root of {number} is approx. {nth_guess}')
-#calling the function
+#Calling the function
 sqrt()
