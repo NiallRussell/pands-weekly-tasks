fcf536638e23742891355541d77fa9535de45096
NiallRussell <77107667+NiallRussell@users.noreply.github.com>
2024-04-18 11:31:18 +0100

    Update plottask.py

--- plottask.py
+++ plottask.py
@@ -1,11 +1,24 @@
+#This program displays a histogram of 1000 random values with a normal distribution, a mean of 5 and a standard deviation of 2
+#Author: Niall Russell
+
+#Import matplotlib to allow for plotting
 import matplotlib.pyplot as plt
+#Import random to allow for random number generation
 import random
+#Set mean
 mu = 5
+#Set standard deviation
 sigma = 2
+#Create list for values
 data = []
+#Generate 1000 random values
 for i in range (1000):
+    #Normal distribution with defined mean and SD
     value = random.gauss(mu, sigma)
+    #Add each value to list
     data.append(value)
 
+#Plot a histogram of values
 plt.hist(data)
+#Display histogram
 plt.show()
