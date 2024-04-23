b57ad4fdd8ba35109cbe93c575c95144a484e81b
Niall.Russell <niall.russell@ul.ie>
2024-04-23 10:29:27 +0100

    labels

--- plottask.py
+++ plottask.py
@@ -5,12 +5,14 @@
 import matplotlib.pyplot as plt
 #Import random to allow for random number generation
 import random
+
 #Set mean
 mu = 5
 #Set standard deviation
 sigma = 2
 #Create list for values
 data = []
+
 #Generate 1000 random values
 for i in range (1000):
     #Normal distribution with defined mean and SD
@@ -20,5 +22,12 @@ for i in range (1000):
 
 #Plot a histogram of values
 plt.hist(data)
+#Assign label to x-axis
+plt.xlabel("Value")
+#Assign label to y-axis
+plt.ylabel("Count")
+#Assign title
+plt.title("Normal distrubution of pseudorandom values")
+
 #Display histogram
 plt.show()
